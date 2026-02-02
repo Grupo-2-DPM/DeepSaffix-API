f11d957940a8e8d480907332a3702b16ad602b8f
Omar Jhon <jhonomar109@gmail.com>
2026-02-01 21:44:55 -0500

    feat(usuarios): anadir modulos de usuarios y perfil

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,20 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 import { SimulationModule } from './simulation/simulation.module';
+import { PerfilAcademicoModule } from './perfil-academico/perfil-academico.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env',}), AuthModule, SimulationModule,],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    AuthModule,
+    UsuariosModule,
+    PerfilAcademicoModule,
+    SimulationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
